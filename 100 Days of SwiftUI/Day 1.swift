d046b30b33a0eeb887b87360b8fe9ea8773c2fd1
PatoMaury <patriciomaury00@gmail.com>
2026-09-16 19:26:40 -0600

    Initial Commit

---
+++ 100 Days of SwiftUI/Day 1.swift
@@ -0,0 +1,53 @@
+import Playgrounds
+
+// How to create variables and constants
+#Playground {
+    var greeting = "Hello Playground"
+    print(greeting)
+    
+    var name = "Ted"
+    name = "Rebecca"
+    name = "Keeley"
+    
+    let character = "Daphne" // This is the way to create a constant.
+    
+    var playerName = "Arda"
+    print(playerName)
+    
+    playerName = "Jude"
+    print(playerName)
+    
+    playerName = "Kylian"
+    print(playerName)
+    
+    let managerName = "Michael"
+    let dogBreed = "Border Collie"
+    let meaningOfLife = "How many roads must a man walk down?"
+}
+
+// How to create strings
+#Playground {
+    let actor = "Denzel Washington"
+    let filename = "paris.jpg"
+    let quote = "Then he tapped a sign saying \"Believe\" and walked away"
+    let movie = """
+        A day in
+        the life of an
+        Apple engineer
+        """
+    
+    print(actor.count)
+    
+    let nameLenght = actor.count
+    print(nameLenght)
+    
+    print(filename.uppercased())
+    
+    print(movie.hasPrefix("A day"))
+    print(filename.hasSuffix(".jpg"))
+}
+
+// How to store whole numbers
+#Playground {
+    
+}
---
+++ 100 Days of SwiftUI/README.md
@@ -0,0 +1,26 @@
+# 100 Days of SwiftUI
+
+Notes and progress repository following the [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui) course by Paul Hudson (Hacking with Swift).
+
+## Goal
+
+Learn Swift and SwiftUI from scratch to build my own iOS apps and, eventually, offer freelance development services.
+
+## Structure
+
+Each folder corresponds to a day of the course:
+
+Day 1/
+Day 2/
+Day 3/
+...
+
+## Progress
+
+- [ ] Days 1-15: Swift fundamentals
+- [ ] Days 16-40: SwiftUI basics
+- [ ] Days 41-100: Practice projects
+
+## Notes
+
+Any questions, solved challenges, or important concepts are documented inside the corresponding day's folder.
